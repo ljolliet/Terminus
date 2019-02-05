9292caf65b06a82557a01fd045a68743d1cecf51
Lilian Gallon <swellchaser33@gmail.com>
2019-02-05 17:33:49 +0100

    add basic parser, documentation is needed

---
+++ js/parser.js
@@ -0,0 +1,88 @@
+/**
+ * TODO: doc
+ */
+class Parser{
+
+    /**
+     * 
+     * @param {string} command the command that the user typed.
+     * @param {boolean} verbose if true, it writes information about parsing
+     */
+    constructor(command, verbose){
+        this.command = command;
+        this.verbose = verbose;
+
+        this.commands = [];
+    }
+
+    /**
+     * TODO
+     */
+    parseCommand(){
+        if(this.command.length == 0){
+            if(this.verbose){
+                console.log("[Parser] The command is empty");
+            }
+        }else{
+            // We devide the command in multiple commands if there was a pipe
+            let cmds = this.command.split("|");
+            
+            // For each commands between the pipe, we will add its details to the array
+            for(let i = 0; i < cmds.length ; i ++){
+                let args = cmds[i].split(" ");
+
+                // Shift removes the first item and returns the removed item
+                let cmdMain = args.shift();
+                let cmdArgs = args;
+
+                let cmdObj = {
+                    main: cmdMain,
+                    args: cmdArgs
+                };
+
+                this.commands.push(cmdObj);
+            }
+
+            if(this.verbose){
+                console.log("Command list :" + this.commands);
+            }
+        }
+    }
+
+    /**
+     * @returns the command.
+     */
+    getCommand(){
+        return this.command;
+    }
+
+    /**
+     * @returns the main command.
+     */
+    getMainCommand(){
+        return this.mainCmd;
+    }
+
+    /**
+     * @returns an array with all the command arguments.
+     * The size is 0 if there is no arguments.
+     */
+    getArguments(){
+        return this.arguments;
+    }
+}
+
+/**
+ * THIS IS A TEST FUNCTION :
+ * It retrieves the content of the input of id "input", and parses it.
+ * @param {key event} event 
+ */
+function runParser(e){
+    if (e.keyCode == 13) {
+        var input = document.getElementById("input");
+        let command = input.value;
+        
+        let parser = new Parser(command, true);
+        parser.parseCommand();
+    }
+}
