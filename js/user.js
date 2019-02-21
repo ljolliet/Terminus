040d83c880db17aa1f7a962782592c7c419780b2
ljolliet <louis.jolliet@orange.fr>
2019-02-21 16:55:40 +0100

    entity and user imp

--- js/user.js
+++ js/user.js
@@ -1,3 +1,31 @@
-class User{
-    
+class User {
+
+    constructor(login) {
+        this.login = login;
+    }
+    /**
+     * @return {Item[]} Items owned by the user.
+     */
+    getItems() { return this.items; }
+    /**
+     * @return {String[]} Quests name finished by the user.
+     */
+    getTrophies() { return this.trophies; }
+    /**
+     * @return {Command[]} Commands allowed.
+     */
+    getCommandsAuthorized() { return this.commandsAuthorized; }
+    /**
+     * @param {Item} item To add to user items.
+     */
+    addItem(item) { this.items.push(item); }
+    /**
+     * @param {String} trophy To add to user trophies. 
+     */
+    addTrophy(trophy) { this.trophies.push(trophy); }
+    /**
+     * @param {Command} command To add to user allowed commands.
+     */
+    addCommand(command) { this.commandsAuthorized.push(command); }
+
 }
