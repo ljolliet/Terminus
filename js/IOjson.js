6aefd2cd98f77a356b66580a70c2c44f00870506
unknown <n.desc64@gmail.com>
2019-05-01 11:21:07 +0200

    add documentation in IOjson

--- js/IOjson.js
+++ js/IOjson.js
@@ -15,13 +15,17 @@ class IOjson{
 
     // ---------------------------------------- Get all place elements ------------------------------- //
     /**
-     * return all places of the _file.json
+     * @returns {Object[]} Object[] that contains all json's Places
      */
     getAllPlaces(){
         return this._json.World.Places;
     }
 
 
+    /**
+     * @param {int} id of the Place
+     * @returns {Object} Object corresponding to the place with id = id
+     */
     getPlace(id){
         let places = this.getAllPlaces();
         let place = places.find( (place) => {
@@ -30,22 +34,42 @@ class IOjson{
         return place;
     }
 
+    /**
+     * @param {int} id of the Place
+     * @returns {Object[]} Object[] corresponding to all quests available in the Place with id = id
+     */
     getPlaceQuests(id){
         return this.getPlace(id).quests;
     }
 
+    /**
+     * @param {int} id of the Place
+     * @returns {Object[]} Object[] corresponding to all places accessible from the Place with id = id
+     */
     getAccessiblePlace(id){
         return this.getPlace(id).next_Place;
     }
 
+    /**
+     * @param {int} id of the Place
+     * @returns {Object[]} Object[] corresponding to all PNJs present in the Place with id = id
+     */
     getPlacePNJ(id){
         return this.getPlace(id).pnj;
     }
 
+    /**
+     * @param {int} id of the Place
+     * @returns {Object[]} Object[] corresponding to all Items present in the Place with id = id
+     */
     getPlaceItems(id){
         return this.getPlace(id).items;
     }
 
+    /**
+     * @param {int} id of the Place
+     * @returns {Object[]} Object[] corresponding to all scripts present in the Place with id = id
+     */
     getPlaceScript(id){
         return this.getPlace(id).script;
     }
@@ -53,12 +77,16 @@ class IOjson{
     // ---------------------------------------- Get all quests elements ------------------------------- //
 
     /**
-     * return all quests of the _file.json
+     * @returns {Object[]} Object[] that contains all json's Places
      */
     getAllQuests(){
         return this._json.World.Quests;
     }
 
+    /**
+     * @param {int} id of the Quest
+     * @returns {Object} Object corresponding to the quest with id = id
+     */
     getQuest(id){
         let quests = this.getAllQuests();
         return quests.find((quest) => {
@@ -66,115 +94,155 @@ class IOjson{
         });
     }
 
+    /**
+     * @param {int} id of the Quest
+     * @returns {Object[]} Object[] corresponding to all quests required in the Quest with id = id
+     */
     getQuestRequirements(id){
         return this.getQuest(id).requirement;
     }
 
+    /**
+     * @param {int} id of the Quest
+     * @returns {String[]} String[] that contains all commands rewards in the Quest with id = id
+     */
     getQuestCommandsRewards(id){
         return this.getQuest(id).commands_Rewards;
     }
 
+    /**
+     * @param {int} id of the Quest
+     * @returns {String} Status of the Quest with id = id
+     */
     getQuestStatus(id){
         return this.getQuest(id).status;
     }
 
+    /**
+     * @param {int} id of the Quest
+     * @returns {String} Text start of the Quest with id = id
+     */
     getQuestTextStart(id){
         return this.getQuest(id).text_Start;
     }
 
+    /**
+     * @param {int} id of the Quest
+     * @returns {String} Text end of the Quest with id = id
+     */
     getQuestTextEnd(id){
         return this.getQuest(id).text_End;
     }
 
+    /**
+     * @param {int} id of the Quest
+     * @returns {String[]} String[] that contains all commands required to end the Quest with id = id
+     */
     getQuestCommandRequired(id){
         return this.getQuest(id).commandRequired;
     }
 
     // ---------------------------------------- Get all user elements ------------------------------- //
 
+    /**
+     * @returns {Object} Object that contains all user's information
+     */
     getUser(){
         return this._json.World.User;
     }
 
+    /**
+     * @returns {String[]} String[] that contains all commands available for the user at the begining of the game
+     */
     getUserCommands(){
         return this.getUser().commands;
     }
 
+    /**
+     * Set the user commands available. Transform string from getUserCommands() into a COMMAND_TYPE in order to add the command to user.
+     */
     setUserCommands(user) {
         this.getUserCommands().forEach((command) => {
 
-        switch (command) {
-            case "exit":
-                user.addCommand(COMMAND_TYPE.EXIT);
-                break;
+            switch (command) {
+                case "exit":
+                    user.addCommand(COMMAND_TYPE.EXIT);
+                    break;
 
-            case "help":
-                user.addCommand(COMMAND_TYPE.HELP);
-                break;
+                case "help":
+                    user.addCommand(COMMAND_TYPE.HELP);
+                    break;
 
-            case "cd" :
-                user.addCommand(COMMAND_TYPE.CD);
-                break;
+                case "cd" :
+                    user.addCommand(COMMAND_TYPE.CD);
+                    break;
 
-            case "cat" :
-                user.addCommand(COMMAND_TYPE.CAT);
-                break;
+                case "cat" :
+                    user.addCommand(COMMAND_TYPE.CAT);
+                    break;
 
-            case "ls" :
-                user.addCommand(COMMAND_TYPE.LS);
-                break;
+                case "ls" :
+                    user.addCommand(COMMAND_TYPE.LS);
+                    break;
 
-            case "mv" :
-                user.addCommand(COMMAND_TYPE.MV);
-                break;
+                case "mv" :
+                    user.addCommand(COMMAND_TYPE.MV);
+                    break;
 
-            case "tree" :
-                user.addCommand(COMMAND_TYPE.TREE);
-                break;
+                case "tree" :
+                    user.addCommand(COMMAND_TYPE.TREE);
+                    break;
 
-            case "grep" :
-                user.addCommand(COMMAND_TYPE.GREP);
-                break;
+                case "grep" :
+                    user.addCommand(COMMAND_TYPE.GREP);
+                    break;
 
-            case "jobs" :
-                user.addCommand(COMMAND_TYPE.JOBS);
-                break;
+                case "jobs" :
+                    user.addCommand(COMMAND_TYPE.JOBS);
+                    break;
 
-            case "clear" :
-                user.addCommand(COMMAND_TYPE.CLEAR);
-                break;
+                case "clear" :
+                    user.addCommand(COMMAND_TYPE.CLEAR);
+                    break;
 
-            case "man" :
-                user.addCommand(COMMAND_TYPE.MAN);
-                break;
+                case "man" :
+                    user.addCommand(COMMAND_TYPE.MAN);
+                    break;
 
-            case "yes" :
-                user.addCommand(COMMAND_TYPE.YES);
-                break;
+                case "yes" :
+                    user.addCommand(COMMAND_TYPE.YES);
+                    break;
 
-            case "chmod" :
-                user.addCommand(COMMAND_TYPE.CHMOD);
-                break;
+                case "chmod" :
+                    user.addCommand(COMMAND_TYPE.CHMOD);
+                    break;
 
-            case "touch" :
-                user.addCommand(COMMAND_TYPE.TOUCH);
-                break;
+                case "touch" :
+                    user.addCommand(COMMAND_TYPE.TOUCH);
+                    break;
 
-            case ">" :
-                user.addCommand(COMMAND_TYPE.WRITE);
-                break;
+                case ">" :
+                    user.addCommand(COMMAND_TYPE.WRITE);
+                    break;
 
-            case ">>" :
-                user.addCommand(COMMAND_TYPE.APPEND);
-                break;
+                case ">>" :
+                    user.addCommand(COMMAND_TYPE.APPEND);
+                    break;
 
-            default :
-                user.addCommand(COMMAND_TYPE.UNKNOWN);
-                break;
-        }
+                default :
+                    user.addCommand(COMMAND_TYPE.UNKNOWN);
+                    break;
+            }
         });
     }
 
+    /**
+     * @param {String} login chosen by the user at the beginning of the game
+     * @param {Place[]} PlaceTab Tab that contains all the Places created.
+     * @param {Quest[]} QuestTab Tab that contains all the Quests created.
+     * Initilize the world. Recover all objects from IOjson's functions and create object Places and Quests.
+     * Add this object into PlaceTab and QuestTab in order that they will be accessible into main.js.
+     */
     init(login, PlaceTab, QuestTab){
         // translate quests from json
         for (let q of this.getAllQuests()) {
@@ -335,7 +403,7 @@ class IOjson{
 
     /**
      * Search in PlaceTab and return a place
-     * @param {int} id The id of the place
+     * @param {int} id The id of the Place
      * @returns {Place}
      */
     findPlace(id){
@@ -350,7 +418,7 @@ class IOjson{
      * @param {int} id The id of the Quest
      * @returns {Quest}
      */
-     findQuest(id){
+    findQuest(id){
 
         return QuestTab.find(function (quest) {
             return (quest.id === id);
